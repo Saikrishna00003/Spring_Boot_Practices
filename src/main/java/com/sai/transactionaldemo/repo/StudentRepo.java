142e3e93603b3928fcf23a8b6643d96c7fbb9f6d
GANDLA VENKATA SAI KRISHNA <gvsaikrishna003@gmail.com>
2025-01-02 12:55:32 +0530

    Delete src directory

--- src/main/java/com/sai/transactionaldemo/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sai.transactionaldemo.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.sai.transactionaldemo.entity.Student;
-
-public interface StudentRepo extends MongoRepository<Student, Integer>{
-
-}
